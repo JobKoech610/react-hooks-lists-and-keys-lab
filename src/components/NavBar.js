1580a69bb18cc9896d1648cb7fad07de901c5464
Job koech <jobkoech610@gmail.com>
2023-04-27 02:43:13 +0300

    made changes on navBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return (
-    <nav>
-    {<a href="#home">home</a>}
-    {<a href="#about">about</a>}
-    {<a href="#projects">projects</a>}
-    </nav>
-  )
- 
+let link = links.map((link)=>{
+  return <a key={link}href={`#${link}`}>{link}</a>
+})
+  return <nav>
+     {link}
+  </nav>;
+   
 }
 
 export default NavBar;
